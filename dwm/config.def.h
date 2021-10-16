2c5f89d2ac7749f9c7577d764970af54367095c5
liubang <it.liubang@gmail.com>
2021-10-16 19:30:23 +0800

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -62,12 +62,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "/home/liubang/.config/rofi/bin/launcher_colorful", NULL };
-static const char *termcmd[]  = { "/usr/local/bin/alacritty", NULL };
+static const char *termcmd[]  = { "/opt/app/bin/kitty", NULL };
+static const char *lockcmd[]  = { "/usr/bin/i3lock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
