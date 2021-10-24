6b0569f06a60edb29e79488716191063772168db
liubang <it.liubang@gmail.com>
2021-10-25 00:30:53 +0800

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -35,7 +35,8 @@ static char *colors[][3] = {
 // };
 
 /* tagging */
-static const char *tags[] = {"", "", "", "", "", "", "", "", ""};
+// static const char *tags[] = {"", "", "", "", "", "", "", "", ""};
+static const char *tags[] = { " ", " ", " ", " ", " ", "  ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -55,11 +56,14 @@ static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+
+#include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+    { "HHH",      grid },
 };
 
 /* key definitions */
@@ -98,6 +102,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
