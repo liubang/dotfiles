b51d05d68da0cdff020c50a048e989146f238e8f
liubang <it.liubang@gmail.com>
2022-05-15 00:54:51 +0800

    update

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -15,6 +15,6 @@ run nm-applet &
 run fcitx5 -d
 run picom --experimental-backends -b
 
-feh --no-fehbg --bg-scale "$HOME/.config/bspwm/wallpaper.jpg"
+feh --no-fehbg --bg-scale "$HOME/.config/bspwm/wallpaper.png"
 bash $HOME/.config/bspwm/polybar/launch.sh
 xsetroot -cursor_name left_ptr
