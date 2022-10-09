a8996b4d358d46e80c2913f6787b4a3cb82b9db7
liubang <it.liubang@gmail.com>
2022-10-09 23:07:29 +0800

    update

--- config/bspwm/polybar/scripts/cava.sh
+++ config/bspwm/polybar/scripts/cava.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-
 bar="▁▂▃▄▅▆▇█"
 dict="s/;//g;"
 
 # creating "dictionary" to replace char with bar
 i=0
-while [ $i -lt ${#bar} ]
-do
-    dict="${dict}s/$i/${bar:$i:1}/g;"
-    i=$((i=i+1))
+while [ $i -lt ${#bar} ]; do
+  dict="${dict}s/$i/${bar:$i:1}/g;"
+  i=$((i = i + 1))
 done
 
 # make sure to clean pipe
 pipe="/tmp/cava.fifo"
 if [ -p $pipe ]; then
-    unlink $pipe
+  unlink $pipe
 fi
 mkfifo $pipe
 
@@ -23,18 +21,18 @@ mkfifo $pipe
 config_file="/tmp/polybar_cava_config"
 echo "
 [general]
-bars = 10
+bars = 12
 [output]
 method = raw
 raw_target = $pipe
 data_format = ascii
 ascii_max_range = 7
-" > $config_file
+" >$config_file
 
 # run cava in the background
 cava -p $config_file &
 
 # reading data from fifo
 while read -r cmd; do
-    echo $cmd | sed $dict
-done < $pipe
+  echo $cmd | sed $dict
+done <$pipe
